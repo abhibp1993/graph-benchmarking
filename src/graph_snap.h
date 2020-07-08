fb96e91c01b30c07dba8df18391a0d6cd7d06135
Gong Yaru <gyr651469689@gmail.com>
2020-07-08 17:28:30 -0400

    get in edge

--- src/graph_snap.h
+++ src/graph_snap.h
@@ -19,63 +19,71 @@ public:
         // TODO: Check whether node with same name is already added.
 
         // Note: For benchmarking purposes, we know by design repetition is avoided.
-        if(m_Nodes.find(name) != m_Nodes.end()){
+        if(m_NodesId.find(name) != m_NodesId.end()){
             std::cout << "Node with name " << name << " is already existed." << std::endl;
-            return m_Nodes.find(name)->second;
+            return m_Nodes[m_NodesId.find(name)->second];
         }
 
         int nid = m_Graph->AddNode();
         Node n = Node(nid, name);
-        m_Nodes.insert({name, n});
+        m_NodesId.insert({name, nid});
+        m_Nodes[nid] = n;
         return n;
     }
 
     Edge AddEdge(std::string name, std::shared_ptr<Node> srcN, std::shared_ptr<Node> tgtN) {
 
-        if(m_Edges.find(name) != m_Edges.end()){
+        if(m_EdgesId.find(name) != m_EdgesId.end()){
             std::cout << "Edge with name " << name << " is already existed." << std::endl;
-            return m_Edges.find(name)->second;
+            return m_Edges[m_EdgesId.find(name)->second];
         }
 
         int eId = m_Graph->AddEdge(srcN->getNId(), tgtN->getNId());
-//        std::shared_ptr<Node> src = std::make_shared<Node>(srcN);
-//        std::shared_ptr<Node> tgt = std::make_shared<Node>(tgtN);
-
-//        Edge edge = Edge(eId, src, tgt);
         Edge edge = Edge(eId, srcN, tgtN);
-        m_Edges.insert({name, edge});
+        m_EdgesId.insert({name, eId});
+        m_Edges[eId] = edge;
         return edge;
 
     }
 
 
-//    std::vector<Edge> GetInEdges(std::shared_ptr<Node> u) {
-//        Node n = *u;
-//        std::vector<Edge> inEdges;
-//        int inDeg = n->GetInDeg();
-//        for(int i  = 0; i < inDeg; i++){
-//            int eId = m_Graph->GetInEId(i, n);
-//            inEdges.push_back(m_Egdes.find(eId).second);
-//        }
-//        return inEdges;
-//    }
-//
-//    std::vector<Edge> GetOutEdges(std::shared_ptr<Node> u) {
-//        Node n = *u;
-//        std::vector<Edge> outEdges;
-//        int outDeg = n->GetOutDeg();
-//        for(int i  = 0; i < outDeg; i++){
-//            int eId = m_Graph->GetOutEId(i, n);
-//            outEdges.push_back(m_Egdes.find(eId).second);
-//        }
-//        return outEdges;
-//
-//    }
+    std::vector<Edge> GetInEdges(std::shared_ptr<Node> u) {
+        Node n = *u;
+        int nId = n.getNId();
+        Node n_snap = m_Graph->GetNode(nId)
+        std::vector<Edge> inEdges;
+
+
+        //compile error here
+
+        int inDeg = (m_Graph->GetNode(nId))->GetInDeg();
+        for(int i  = 0; i < inDeg; i++){
+            int eId = m_Graph->GetInEId(i, n);
+            inEdges.push_back(m_Egdes.find(eId).second);
+        }
+        return inEdges;
+    }
+
+    std::vector<Edge> GetOutEdges(std::shared_ptr<Node> u) {
+        Node n = *u;
+        std::vector<Edge> outEdges;
+        int outDeg = n->GetOutDeg();
+        for(int i  = 0; i < outDeg; i++){
+            int eId = m_Graph->GetOutEId(i, n);
+            outEdges.push_back(m_Egdes.find(eId).second);
+        }
+        return outEdges;
+
+    }
+
+
 
 private:
     PNEGraph m_Graph;
-    std::unordered_map<std::string, Node> m_Nodes;
-    std::unordered_map<std::string, Edge> m_Edges;
+    std::unordered_map<std::string, int> m_NodesId;
+    std::unordered_map<std::string, int> m_EdgesId;
+    std::vector<Node> m_Nodes;
+    std::vector<Edge> m_Edges;
 };
 
 
